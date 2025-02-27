c4725facf9b117ca18243ade5a2aef303b361270
gtlab2023 <chenglichengwai2020@gmail.com>
2025-02-27 20:29:09 +0800

    feat: 添加UI测试

---
+++ docs/ui_report/config.js
@@ -0,0 +1,23 @@
+report({
+  "testSuite": "BackstopJS",
+  "tests": [
+    {
+      "pair": {
+        "reference": "../../backstop_data/bitmaps_reference/backstop_default_BackstopJS_Homepage_0_body_0_table.png",
+        "test": "../../backstop_data/bitmaps_test/20250227-181037/backstop_default_BackstopJS_Homepage_0_body_0_table.png",
+        "selector": "body",
+        "fileName": "backstop_default_BackstopJS_Homepage_0_body_0_table.png",
+        "label": "BackstopJS Homepage",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "https://garris.github.io/BackstopJS/",
+        "referenceUrl": "",
+        "expect": 0,
+        "viewportLabel": "table",
+        "error": "Reference file not found /Users/gaotian/Documents/blog/前端秘籍/前端架构/实践/gt-web3-spa/backstop_data/bitmaps_reference/backstop_default_BackstopJS_Homepage_0_body_0_table.png"
+      },
+      "status": "fail"
+    }
+  ],
+  "id": "backstop_default"
+});
